ddb2fe22c163dcfc4c2f1b4db4e7946e09be5f3c
CharlyWells <wellingtoncharlottenaaodarley@gmail.com>
2024-02-05 20:17:13 +0000

    Added new files

---
+++ 1-first-program/1-first.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+#This program says Hello and asks for my name 
+print('Hello World')
+print('What is your name?')
+myName = input ('Enter your name: ')
+print('It is good to meet you' + myName)
+print('The length of your name is: ')
+print(len(myName)) #Outputs length of the value stores in the variable
+print('What is your age?') # ask for their age
+myAge = input ('Enter your age: ')
+print('You are ' + myAge )
