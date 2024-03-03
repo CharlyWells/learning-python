b163749a3470524793be61cdb131e0335144c9fb
CharlyWells <wellingtoncharlottenaaodarley@gmail.com>
2024-03-03 16:11:45 +0000

    Added new files

--- functions/21-functions.py
+++ functions/21-functions.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+#In this very first programming challenge, your goal is to implement a program that reads two digits from the standard input and prints their sum to the standard output.  We start from this ridiculously simple problem to show you the pipeline of reading the problem statement, designing an algorithm, implementing it,testing and debugging your program.
 
 def sum_of_two_digits(first_digit, second_digit):
     return first_digit + second_digit
