06bfaf8db72b19af1bacaf866bae6b2ed3d9c820
CharlyWells <wellingtoncharlottenaaodarley@gmail.com>
2024-03-03 21:04:33 +0000

    Added new files

---
+++ hello-world/1-hello-world.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+#using f-strings 
+number = 98
+print(f"{number:d}", "Battery street")
---
+++ hello-world/2-hello-world.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+#F-strings
+number = 3.14159
+
+print(f"Float: {number:.2f}")
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
