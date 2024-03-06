2e08a3c56188acdf038ea181b6288afd2262fa7a
CharlyWells <wellingtoncharlottenaaodarley@gmail.com>
2024-03-06 15:56:19 +0000

    Added new files

---
+++ lists/6-list.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+#using in operator whether a value is in a list
+print('howdy' in ['hello','hi','howdy', 'heyas'])
---
+++ lists/8-list.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+#using not in operator in lists
+
+myPets = ['dog','cat','frog']
+print('dog' not in myPets)
